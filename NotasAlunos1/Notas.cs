21538bb48a2343e4cd0874295e2f99da8f75ef2f
João Junior <batistasousajoao@gmail.com>
2023-07-31 08:52:06 -0300

    Adicionar arquivos de projeto.

---
+++ NotasAlunos1/Notas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotasAlunos1
+{
+    public class Notas
+    {
+        public string Nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double CalculoNotas()
+        {
+            double n = (nota1 + nota2 + nota3);
+            return n;
+        }
+
+        public bool Aprovado()
+        {
+
+            if (CalculoNotas() >= 60.00)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.00 - CalculoNotas();
+            }
+
+        }
+
+
+    }
+}
---
+++ NotasAlunos1/Program.cs
@@ -0,0 +1,38 @@
+﻿using NotasAlunos1;
+using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace alunos
+{
+    public class alunos1
+    {
+        static void Main(string[] args)
+        {
+
+            Notas notas = new Notas();
+
+            Console.WriteLine("Digite o nome do aluno: ");
+            Console.Write("Nome: ");
+            notas.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as notas dos bimestres:");
+            notas.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            notas.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            notas.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (notas.Aprovado())
+            {
+                Console.WriteLine("Você foi aprovado!");
+                Console.WriteLine("Sua nota é: " + notas.CalculoNotas().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            else
+            {
+                Console.WriteLine("Você foi reprovado!");
+                Console.WriteLine("Sua nota foi: " + notas.CalculoNotas().ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Faltaram " + notas.NotaRestante().ToString("F2", CultureInfo.InvariantCulture) + " pontos");
+            }
+            
+        }
+    }
+}
